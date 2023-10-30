2599491cde2a0af2b1dea13c3fe2955e73483330
m926n810 <m926n810@ku.edu>
2023-10-30 14:03:14 -0500

    udp

---
+++ UDP/UDP-client.py
@@ -0,0 +1,35 @@
+import socket, struct
+
+FORMAT = "utf-8"
+
+def send_file(remote_ip, remote_port, local_file):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client:
+        client.connect((remote_ip, remote_port))
+
+        with open(local_file, 'rb') as f:
+            file_data = f.read()
+            file_size = len(file_data)
+            file_name = local_file[:20]
+
+            client.sendto(struct.pack('!I', file_size))
+            print(f"server: {client.recvfrom(1024).decode(FORMAT)}")
+
+            client.sendto(struct.pack('20s', file_name.encode(FORMAT)))
+            print(f"server: {client.recvfrom(1024).decode(FORMAT)}")
+
+            client.sendto(struct.pack(f'{file_size}s', file_data))
+            print(f"server: {client.recvfrom(1024).decode(FORMAT)}")
+            
+            print(f"File {local_file} sent.")
+            f.close()
+
+        client.close()
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: python3 TCP-client.py <remote-IP-on-machine-1> <remote-port-on-machine-1> <local-file-on-machine-2-to-transfer>")
+        sys.exit(1)
+
+    remote_ip, remote_port, local_file = sys.argv[1], int(sys.argv[2]), sys.argv[3]
+    send_file(remote_ip, remote_port, local_file)
--- UDP/UDP-server.py
+++ UDP/UDP-server.py
@@ -4,7 +4,7 @@ FORMAT = "utf-8"
 
 def get_local_ip():
     try:
-        temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        temp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         temp_socket.connect(("8.8.8.8", 80)) 
         local_ip = temp_socket.getsockname()[0]
         temp_socket.close()
@@ -17,31 +17,27 @@ def receive_file(server_port, ip):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         
         server.bind((ip, server_port))
-        server.listen()
         print(f"Server ({ip}) is listening on port {server_port}...")
         while True:
-            conn, addr = server.accept()
-            print(f'Connected by {addr}')
 
-            (file_size,) = struct.unpack("!I", conn.recv(4))  # 4 bytes for file size, 
-            conn.send("Receive filesize".encode(FORMAT))
+            (file_size,) = struct.unpack("!I", server.recvfrom(4))  # 4 bytes for file size, 
+            server.sendto("Receive filesize".encode(FORMAT))
 
-            file_name = conn.recv(20).decode(FORMAT).strip('\x00') # 20 bytes for file name
+            file_name = server.recvfrom(20).decode(FORMAT).strip('\x00') # 20 bytes for file name
             print(f"Receive {file_name} with size {file_size}")
-            conn.send("Receive filename".encode(FORMAT))
+            server.sendto("Receive filename".encode(FORMAT))
 
             if not os.path.exists("received_files"):
                 os.makedirs("received_files")
 
             with open(f'received_files/{str(file_name)}', 'wb') as received_file:
-                data = conn.recv(file_size)
+                data = server.recvfrom(file_size)
                 received_file.write(data)
                 received_file.close()
-                conn.send("Received filedata".encode(FORMAT))
+                server.sendto("Received filedata".encode(FORMAT))
                 print(f"File {file_name} received. Stored as received_files/{file_name}")
                 
-            conn.close()
-            print(f'{addr} disconnected.')
+            server.close()
 
 if __name__ == "__main__":
     import sys
